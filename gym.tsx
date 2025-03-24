f39ad1330f0e8d089dc9a8c0eceddab397732ebf
Gabriela Pedreros <80491865+gabrielapedreros@users.noreply.github.com>
2025-03-24 14:11:49 -0400

    Add files via upload

---
+++ gym.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="gym" tilewidth="32" tileheight="32" tilecount="1792" columns="56">
+ <image source="gym.png" width="1792" height="1024"/>
+</tileset>
